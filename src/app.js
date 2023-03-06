0b6364b96f1e6c023cf6007c64be5c3a3436f4e7
catalinasehrt <catalina.dali94@gmail.com>
2023-03-06 14:39:05 +0000

    carta

--- src/app.js
+++ src/app.js
@@ -7,13 +7,33 @@ import "./assets/img/4geeks.ico";
 
 //write your code here
 window.onload = () => {
-  document.querySelector(".card").classList.add("♥");
-  setTimeout(() => {
-    document.querySelector(".card").classList.remove("♥");
-    document.querySelector(".card").classList.add("♣");
-  }, 1000);
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
 };
 
-let numeroRandom () {
-  let numero =  Math.random(Math.random() * 12);
-}
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+
+  return numbers[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+  let suit = ["♥", "♦", "♠ ", "♣"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+};
